632d0fb26701a1c252c77092d3b943cc688427f0
cr542 <christian.reisenauer@gmx.at>
2021-01-12 21:53:15 +0100

    Added getter and toString method

--- src/main/java/model/persons/Medic.java
+++ src/main/java/model/persons/Medic.java
@@ -6,28 +6,20 @@ import model.schedules.Roster;
 import java.time.LocalDate;
 
 public class Medic extends Employee {
+
+	// A counter for all the examinations that a Medic has carried out. For statistic purposes.
 	private final int examCounter = 0;
 
 	public Medic(String firstname, String lastname, LocalDate dateOfBirth, Address address, int staffID, String email, String phone, Roster roster) {
 		super(firstname, lastname, dateOfBirth, address, staffID, email, phone, roster);
 	}
 
-
-	public void createExamination() {
-
+	public int getExamCounter() {
+		return examCounter;
 	}
 
-	public void completeExamination() {
-//		TODO stuff
-//		this.examCounter++;
+	@Override
+	public String toString() {
+		return "[Medic] "+super.toString()+" | Total examinations: "+examCounter;
 	}
-	
-	public void confirmExamination(Examination e, Employee emp) {
-//		TODO Change "Employee" to the class for policemen in field
-	}
-	
-	public void generateAttest(Examination e, Employee emp) {
-//		TODO Change "Employee" to the class for policemen in field
-	}
-	
 }
