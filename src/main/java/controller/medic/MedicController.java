878e201466ffa029e043542cb08e2cdf0f040a7a
cr542 <christian.reisenauer@gmx.at>
2021-01-12 21:48:19 +0100

    Update MedicController.java

--- src/main/java/controller/medic/MedicController.java
+++ src/main/java/controller/medic/MedicController.java
@@ -1,5 +1,28 @@
 package controller.medic;
 
+import model.events.Examination;
+import model.persons.Employee;
+
 public class MedicController {
     // Do stuff here
+    MedicController()  {
+
+    }
+
+    public void createExamination() {
+
+    }
+
+    public void completeExamination() {
+//		TODO stuff
+//		this.examCounter++;
+    }
+
+    public void confirmExamination(Examination e, Employee emp) {
+//		TODO Change "Employee" to the class for policemen in field
+    }
+
+    public void generateAttest(Examination e, Employee emp) {
+//		TODO Change "Employee" to the class for policemen in field
+    }
 }
