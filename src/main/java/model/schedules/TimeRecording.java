9d4924f636e08b777c9e839b3b94a0693f70a92c
Lukas Wais <lukas.wais@outlook.de>
2021-01-12 21:05:34 +0100

    Created some model files and implemented clock in.
    Signed-off-by: Lukas Wais <lukas.wais@jku.at>

--- src/main/java/model/schedules/TimeRecording.java
+++ src/main/java/model/schedules/TimeRecording.java
@@ -1,5 +1,63 @@
 package model.schedules;
 
-public class TimeRecording {
+import model.persons.Employee;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+// TODO ist soll berechnen
+public class TimeRecording {
+    // instead of DB static list
+    private static final List<TimeRecording> timeRecordings = new ArrayList<>();
+    private final Employee employee;
+    private final boolean isOnVacation;
+    private final boolean isSick;
+    private final LocalDateTime date;
+    // TODO check for better solution
+    // @see https://www.baeldung.com/java-add-hours-date
+    private final int workingTime; // Sollzeit
+
+    public TimeRecording(Employee employee, boolean isOnVacation, boolean isSick, LocalDateTime date, int workingTime) {
+        this.employee = employee;
+        this.isOnVacation = isOnVacation;
+        this.isSick = isSick;
+        this.date = date;
+        this.workingTime = workingTime;
+    }
+
+    public static List<TimeRecording> getTimeRecordings() {
+        return timeRecordings;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public boolean isOnVacation() {
+        return isOnVacation;
+    }
+
+    public boolean isSick() {
+        return isSick;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public int getWorkingTime() {
+        return workingTime;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeRecording{" +
+                "employee=" + employee +
+                ", isOnVacation=" + isOnVacation +
+                ", isSick=" + isSick +
+                ", date=" + date +
+                ", workingTime=" + workingTime +
+                '}';
+    }
 }
