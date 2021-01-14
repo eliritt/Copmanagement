b24fc47905eacfd0a0f436dfc5e318f80f263a92
Philthy-Phil <phil.ziegelbauer@gmx.at>
2021-01-14 19:38:38 +0100

    quick-setup-inductionPhase

--- src/main/java/model/events/InductionPhase.java
+++ src/main/java/model/events/InductionPhase.java
@@ -1,2 +1,22 @@
-package model.events;public class InductionPhase {
+package model.events;
+
+import model.persons.Employee;
+
+public class InductionPhase {
+
+    private final Employee employee;
+    private final String department;
+    private final String education;
+    private final
+
+
+    public InductionPhase(Employee employee) {
+        this.employee = employee;
+    }
+
+
+
+
+
+
 }
