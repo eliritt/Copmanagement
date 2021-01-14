fc6506316a231f782f9ec0e228a1a7642fd6a61b
Elisabeth Ritt <e.ritt@gmx.at>
2021-01-14 22:03:12 +0100

    Updated Training.java, added CreateTraining-Method @ TrainerController.java

--- src/main/java/controller/trainer/TrainerController.java
+++ src/main/java/controller/trainer/TrainerController.java
@@ -1,5 +1,16 @@
 package controller.trainer;
 
+import java.time.LocalDateTime;
+
+import model.events.Training;
+import model.persons.Trainer;
+
 public class TrainerController {
-    // Do stuff
+    
+	public static void createTraining(int trainingID, String name, String description, Trainer trainer, LocalDateTime dateTime, String place,
+			int maxParticipants, String requirements) {
+		
+    	Training training = new Training(trainingID, name, description, trainer, dateTime, place, maxParticipants, requirements);
+    	Training.trainings.add(training);
+    }
 }
