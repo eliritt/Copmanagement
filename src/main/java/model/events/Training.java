dd01025aa378636214d0e62d1c1e09cae5637ebc
Elisabeth Ritt <e.ritt@gmx.at>
2021-01-14 20:37:03 +0100

    Updated Trainer.java and Training.java
    Signed-off-by: Elisabeth Ritt <e.ritt@gmx.at>

--- src/main/java/model/events/Training.java
+++ src/main/java/model/events/Training.java
@@ -1,5 +1,84 @@
 package model.events;
 
-public class Training {
+import java.time.LocalDateTime;
 
+import model.persons.Trainer;
+
+public class Training {
+	private int trainingID;
+	private String name;
+	private String description;
+	private Trainer trainer;
+	private LocalDateTime dateTime;
+	private String place;
+	private int maxParticipants;
+	private String requirements;
+	
+	public Training(int trainingID, String name, String description, LocalDateTime dateTime, String place,
+			int maxParticipants, String requirements) {
+		this.trainingID = trainingID;
+		this.name = name;
+		this.description = description;
+		this.dateTime = dateTime;
+		this.place = place;
+		this.maxParticipants = maxParticipants;
+		this.requirements = requirements;
+	}
+	
+	public int getTrainingID() {
+		return trainingID;
+	}
+
+	public void setTrainingID(int trainingID) {
+		this.trainingID = trainingID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public int getMaxParticipants() {
+		return maxParticipants;
+	}
+
+	public void setMaxParticipants(int maxParticipants) {
+		this.maxParticipants = maxParticipants;
+	}
+
+	public String getRequirements() {
+		return requirements;
+	}
+
+	public void setRequirements(String requirements) {
+		this.requirements = requirements;
+	}	
+	
 }
