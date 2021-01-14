da9767b36cac9ce46f0a51c764db6af95cff7d08
cr542 <christian.reisenauer@gmx.at>
2021-01-14 21:15:28 +0100

    Implemented Medic class

--- src/main/java/model/persons/Medic.java
+++ src/main/java/model/persons/Medic.java
@@ -1,6 +1,5 @@
 package model.persons;
 
-import model.events.Examination;
 import model.schedules.Roster;
 
 import java.time.LocalDate;
@@ -8,18 +7,23 @@ import java.time.LocalDate;
 public class Medic extends Employee {
 
 	// A counter for all the examinations that a Medic has carried out. For statistic purposes.
-	private final int examCounter = 0;
+	private int examCounter = 0;
 
 	public Medic(String firstname, String lastname, LocalDate dateOfBirth, Address address, int staffID, String email, String phone, Roster roster) {
 		super(firstname, lastname, dateOfBirth, address, staffID, email, phone, roster);
+		examCounter = 0;
 	}
 
 	public int getExamCounter() {
 		return examCounter;
 	}
 
+	public void increaseExamCounter() {
+		examCounter += 1;
+	}
+
 	@Override
 	public String toString() {
-		return "[Medic] "+super.toString()+" | Total examinations: "+examCounter;
+		return "[Medic] "+super.toString()+" | Total examinations: "+examCounter+".";
 	}
 }
