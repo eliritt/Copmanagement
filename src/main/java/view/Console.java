adb940690ec54fea4fb07baaeb1c247d6ede42b2
cr542 <christian.reisenauer@gmx.at>
2021-01-15 16:54:03 +0100

    Removed main method from Console class

--- src/main/java/view/Console.java
+++ src/main/java/view/Console.java
@@ -9,17 +9,4 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Console {
-    public static void main(String[] args) {
-        EmployeeController employeeController = new EmployeeController();
-        // test login
-        employeeController.insertEmployee(new Employee("Karl", "Nehammer", LocalDate.of(1958, 1, 1),
-                new Address(4020, "3B", "Linz", "Peter-Behrens-Platz"), 123, "aaa@bbb.com", "+4312345", null));
-        Employee employee = employeeController.getEmployeeFromDB(123);
-
-        TimeRecordingController timeRecording = new TimeRecordingController();
-        // clock in
-        timeRecording.comeIn(employee, false, false, LocalDateTime.now(), 8);
-
-        System.out.println(timeRecording.getTimeRecording(employee));
-    }
 }
