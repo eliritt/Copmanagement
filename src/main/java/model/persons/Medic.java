93abd46bbe70f2ae19f76c10d26f5a87171741dd
cr542 <christian.reisenauer@gmx.at>
2021-02-16 17:43:12 +0100

    Update Medic.java

--- src/main/java/model/persons/Medic.java
+++ src/main/java/model/persons/Medic.java
@@ -6,24 +6,24 @@ import java.time.LocalDate;
 
 public class Medic extends Employee {
 
-	// A counter for all the examinations that a Medic has carried out. For statistic purposes.
-	private int examCounter = 0;
+    // A counter for all the examinations that a Medic has carried out. For statistic purposes.
+    private int examCounter = 0;
 
-	public Medic(String firstname, String lastname, LocalDate dateOfBirth, Address address, int staffID, String email, String phone, Roster roster) {
-		super(firstname, lastname, dateOfBirth, address, staffID, email, phone, roster);
-		examCounter = 0;
-	}
+    public Medic(String firstname, String lastname, LocalDate dateOfBirth, Address address, int staffID, String email, String phone, Roster roster) {
+        super(firstname, lastname, dateOfBirth, address, staffID, email, phone, roster);
+        examCounter = 0;
+    }
 
-	public int getExamCounter() {
-		return examCounter;
-	}
+    public int getExamCounter() {
+        return examCounter;
+    }
 
-	public void increaseExamCounter() {
-		examCounter += 1;
-	}
+    public void increaseExamCounter() {
+        examCounter += 1;
+    }
 
-	@Override
-	public String toString() {
-		return "[Medic] "+super.toString()+" | Total examinations: "+examCounter+".";
-	}
+    @Override
+    public String toString() {
+        return "[Medic] " + super.toString() + " | Total examinations: " + examCounter + ".";
+    }
 }
