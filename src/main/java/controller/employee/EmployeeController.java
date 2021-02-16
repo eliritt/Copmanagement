2764679c55f7d6d61e88b453ae5874c35f5986e6
cr542 <christian.reisenauer@gmx.at>
2021-02-16 17:42:35 +0100

    Moved Employee DB into Controller

--- src/main/java/controller/employee/EmployeeController.java
+++ src/main/java/controller/employee/EmployeeController.java
@@ -2,13 +2,23 @@ package controller.employee;
 
 import model.persons.Employee;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class EmployeeController {
+
+    private static final Set<Employee> employeeDB = new HashSet<>();
+
     public void insertEmployee(Employee employee) {
-        Employee.getEmployeeDB().add(employee);
+        getEmployeeDB().add(employee);
+    }
+
+    public static Set<Employee> getEmployeeDB() {
+        return employeeDB;
     }
 
     public Employee getEmployeeFromDB(int staffID) {
-        for (Employee employee : Employee.getEmployeeDB()) {
+        for (Employee employee : getEmployeeDB()) {
             if (employee.getStaffID() == staffID) {
                 return employee;
             }
